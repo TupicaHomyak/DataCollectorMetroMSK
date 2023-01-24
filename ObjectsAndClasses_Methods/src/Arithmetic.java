528749b31cbdc79c757e425926c96704c12baf22
Olga Malkova <Vodeviiil@mail.ru>
2023-01-24 19:06:03 +0300

    Не до конца поняла тему, буду ожидать замечаний

--- ObjectsAndClasses_Methods/src/Arithmetic.java
+++ ObjectsAndClasses_Methods/src/Arithmetic.java
@@ -1,3 +1,29 @@
-public class Arithmetic {
 
+public class Arithmetic { //класс  с переменными
+    int a;
+    int b;
+    public Arithmetic(int a, int b) { // Конструктор  с переменными
+        this.a = a;
+        this.b = b;
+    }
+    public static int sum(int a, int b) { //Метод суммирования
+        return a + b;
+    }
+    public static int multiplication(int a, int b) { // Метод произведения
+        return a * b;
+    }
+    public static int maximum(int a, int b) { // Метод поиска максимального
+        if (a > b) {
+            return a;
+        } else {
+            return b;
+        }
+    }
+    public static int minimum(int a, int b) { // Метод поиска минимального
+        if (a < b) {
+            return a;
+        } else {
+            return b;
+        }
+    }
 }
--- ObjectsAndClasses_Methods/src/Main.java
+++ ObjectsAndClasses_Methods/src/Main.java
@@ -1,8 +1,24 @@
 public class Main {
-
     public static void main(String[] args) {
-        Basket basket = new Basket();
-        basket.add("Milk", 40);
-        basket.print("Milk");
+// для проверки класса Basket
+
+        Basket vasyaBasket = new Basket();
+        vasyaBasket.add("молоко", 55, 2);
+        vasyaBasket.add("хлеб", 33, 3);
+        vasyaBasket.print("Корзина Васи: ");
+        System.out.println();
+
+        Basket petyaBasket = new Basket();
+        petyaBasket.add("молоко", 55, 2, 0.3);
+        petyaBasket.add("авокадо", 100, 10, 0.2);
+        petyaBasket.add("кола", 130,4, 2.0);
+        petyaBasket.print("Корзина Пети: ");
+
+// для проверки класса Arithmetic
+        System.out.println();
+        System.out.println( "Сумма чисел = " + Arithmetic.sum(2, 8));
+        System.out.println( "Произведение чисел  = " + Arithmetic.multiplication(2, 8));
+        System.out.println( "Максимальное из двух чисел = " + Arithmetic.maximum(2, 8));
+        System.out.println( "Минимальное из двух чисел = " + Arithmetic.minimum(2, 8));
     }
 }
