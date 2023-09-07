83f170cc5fd1dc03f866968fd4fd31b47a1af887
Olga Malkova <Vodeviiil@mail.ru>
2023-09-07 17:13:08 +0300

    15.14

--- AdvancedOOPFeatures/homework_2/Airport/src/main/java/Main.java
+++ AdvancedOOPFeatures/homework_2/Airport/src/main/java/Main.java
@@ -1,17 +1,30 @@
 import com.skillbox.airport.Airport;
 import com.skillbox.airport.Flight;
-import java.util.Collections;
+import java.time.Instant;
+import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
+
+//TODO В Main реализуйте метод findPlanesLeavingInTheNextTwoHours(),
+// который должен вернуть список рейсов, вылетающих в ближайшие два часа,
+// используя Stream API и lambda-выражения.
 
 public class Main {
     public static void main(String[] args) {
-
-
+        Airport airport1 = Airport.getInstance();
+        findPlanesLeavingInTheNextTwoHours(airport1);
     }
 
     public static List<Flight> findPlanesLeavingInTheNextTwoHours(Airport airport) {
-        //TODO Метод должден вернуть список рейсов вылетающих в ближайшие два часа.
-        return Collections.emptyList();
-    }
 
+        Date nowDate = Date.from(Instant.now());
+        Date andTwoH = Date.from(Instant.now().plusSeconds(7200));
+
+        return airport.getTerminals().stream()
+                        .flatMap(terminal -> terminal.getFlights().stream())
+                                .filter(flight -> flight.getDate().after(nowDate)
+                                && flight.getDate().before((andTwoH))
+                                && flight.getType() == Flight.Type.DEPARTURE)
+                                .collect(Collectors.toList());
+    }
 }
